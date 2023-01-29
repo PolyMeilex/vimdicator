130aa99131f6f4cdf3d742ebaafae7288ccaad63
Lyude Paul <thatslyude@gmail.com>
2023-01-28 21:47:26 -0500

    CI: Change timestamp one last time

--- .github/workflows/dnf-makecache.sh
+++ .github/workflows/dnf-makecache.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # Check if we need to refresh the metadata cache, and signal back to the Github job if so
+timestamp() {
+    date -r /var/cache/dnf/last_makecache +%s.%N
+}
+
 echo "::group::Refreshing metadata"
 if [[ -e /var/cache/dnf/last_makecache ]]; then
-    old_time=$(date --rfc-3339=ns -r /var/cache/dnf/last_makecache)
+    old_time=$(timestamp)
 fi
 dnf makecache --timer
-new_time=$(date --rfc-3339=ns -r /var/cache/dnf/last_makecache)
+new_time=$(timestamp)
 
 echo time=$new_time >> $GITHUB_OUTPUT
 if [[ $old_time == $new_time ]]; then
